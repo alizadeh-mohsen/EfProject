ec5c4b3a4434eee60994cca354dbfc9d022efb98
mohsen <kelarens@gmail.com>
2020-02-05 14:42:11 +0330

    new migration

--- EfProject/Migrations/20200205111044_addingPerishable.Designer.cs
+++ EfProject/Migrations/20200205111044_addingPerishable.Designer.cs
@@ -4,14 +4,16 @@ using EfProject.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EfProject.Migrations
 {
     [DbContext(typeof(HPlusSportsContext))]
-    partial class HPlusSportsContextModelSnapshot : ModelSnapshot
+    [Migration("20200205111044_addingPerishable")]
+    partial class addingPerishable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
